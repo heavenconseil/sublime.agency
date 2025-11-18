51a7d1b6951f6cd5c5fe067b9bdd3c046f4f3517
stefw <sw@magicmorning.net>
2025-11-18 18:12:23 +0100

    Initial commit: Sublime Agency landing page with Liquid Metal shader

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['@paper-design/shaders-react'],
 };
 
 export default nextConfig;
